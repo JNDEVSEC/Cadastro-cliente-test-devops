8a0abb9c3439d7dc43795bd8aba6edf1df0dc46a
Joao Neto <45321218+joaosemrif@users.noreply.github.com>
2025-08-27 16:43:27 -0300

    Update ClienteController.java

--- cliente-cadastro-java/src/main/java/com/exemplo/clientes/ClienteController.java
+++ cliente-cadastro-java/src/main/java/com/exemplo/clientes/ClienteController.java
@@ -1,1 +1,25 @@
+package com.exemplo.clientes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    @GetMapping
+    public List<Cliente> listar() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Cliente salvar(@RequestBody Cliente cliente) {
+        return repository.save(cliente);
+    }
+}
 
