b71aae524ee1ffbfd410f5174c619510a361536e
Joao Neto <45321218+joaosemrif@users.noreply.github.com>
2025-08-27 16:42:41 -0300

    Update ClienteCadastroApplication.java

--- cliente-cadastro-java/src/main/java/com/exemplo/clientes/ClienteCadastroApplication.java
+++ cliente-cadastro-java/src/main/java/com/exemplo/clientes/ClienteCadastroApplication.java
@@ -1,1 +1,12 @@
+package com.exemplo.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteCadastroApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteCadastroApplication.class, args);
+    }
+}
 
